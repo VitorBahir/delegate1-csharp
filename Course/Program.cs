b58d5a382e7b6f939dc508a52e4af62ea965fe5d
Vitor Bahir <vitorb82014@gmail.com>
2023-03-30 17:11:11 -0300

    Multicast delegates

---
+++ Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Course.Services;
+namespace Course
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            // BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            // double result = op(a, b);
+            op.Invoke(a, b);
+        }
+    }
+}
